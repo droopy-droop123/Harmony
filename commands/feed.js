93f283b7f99c9661420982f80b1bef1c95d786b0
Trikanoid <herobrine.raw@gmail.com>
2017-07-11 23:21:19 +0800

    Changed message to msg

--- commands/feed.js
+++ commands/feed.js
@@ -5,25 +5,23 @@ module.exports = {
             version: 1, //current API versions are 1 and 2 
             nsfw: false //a boolean for NSFW switching 
         }); //the constructor arguments are optional 
-        const user = message.mentions.users.first();
+        const user = msg.mentions.users.first();
         WolkeApi.picture("nom").then((result) => {
             if (user) {
                 const embed = new Discord.RichEmbed()
-                    .setTitle(`${message.author.username} fed ${user.username} some good noms`)
+                    .setTitle(`${msg.author.username} fed ${user.username} some good noms`)
                     .setColor("#808080")
                     .setImage(result.url)
-                    .setFooter('©2017 Felix#1330', 'http://orig13.deviantart.net/f7a2/f/2016/343/a/b/isana_yashiro_minimal_icon_by_lol123xb-dar48hx.jpg')
 
-                message.channel.sendEmbed(embed)
+                msg.channel.sendEmbed(embed)
             }
             if (!user) {
                 const embed = new Discord.RichEmbed()
-                    .setTitle(`Tsuda fed the lonely ${message.author.username} some sad noms`)
+                    .setTitle(`Tsuda fed the lonely ${msg.author.username} some sad noms`)
                     .setColor("#808080")
                     .setImage(result.url)
-                    .setFooter('©2017 Felix#1330', 'http://orig13.deviantart.net/f7a2/f/2016/343/a/b/isana_yashiro_minimal_icon_by_lol123xb-dar48hx.jpg')
 
-                message.channel.sendEmbed(embed)
+                msg.channel.sendEmbed(embed)
             }
         }).catch((error) => {
             console.log(error.toString());
