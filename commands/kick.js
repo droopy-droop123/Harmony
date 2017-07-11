9565a4102534bd8fccb87f7f6d93e620630a3880
Trikanoid <herobrine.raw@gmail.com>
2017-07-11 23:18:32 +0800

    Changed message to msg

--- commands/kick.js
+++ commands/kick.js
@@ -1,19 +1,19 @@
 module.exports = {
      main: function(bot, msg) {
-     const mention = message.mentions.users.first();
-	var kickPerms = message.guild.member(bot.user).hasPermission('KICK_MEMBERS')
+     const mention = msg.mentions.users.first();
+	var kickPerms = msg.guild.member(bot.user).hasPermission('KICK_MEMBERS')
 	if (!mention) {
-		message.channel.send('Please mention a user, that you would like to kick.')
+		msg.channel.send('Please mention a user, that you would like to kick.')
 	} else {
 		if (!kickPerms) {
-			message.channel.send("Harmony doesn't have enough permissions to kick any user.")
+			msg.channel.send("Harmony doesn't have enough permissions to kick any user.")
 		} else {
-			var kickable = message.guild.member(mention).kickable
+			var kickable = msg.guild.member(mention).kickable
 			if (!kickable) {
-				message.channel.send(mention + " isn't kickable.")
+				msg.channel.send(mention + " isn't kickable.")
 			} else {
-				message.guild.member(mention).kick()
-				message.channel.send(mention + ' has been kicked.')
+				msg.guild.member(mention).kick()
+				msg.channel.send(mention + ' has been kicked.')
 			}
 		}
 	}
