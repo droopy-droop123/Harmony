8c48e4717eaf0eb444f60b9b63fbc8364412d70d
ExChang3 <worshipleader107@gmail.com>
2017-07-11 02:52:02 -0500

    Delete invite.js

--- invite.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-     main: function(bot, msg) {
-        msg.channel.sendMessage("invite me, and let me be your waifu ^-^ http://harmony.exchang3.xyz")
-    },
-    help: 'Sends an invite link for the bot'
-};
