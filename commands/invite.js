a1119e74acf36767cd5018f99feec50453ef2238
ExChang3 <worshipleader107@gmail.com>
2017-07-11 02:53:25 -0500

    Add files via upload
    fixed bugger

---
+++ commands/invite.js
@@ -0,0 +1,6 @@
+module.exports = {
+     main: function(bot, msg) {
+        msg.channel.sendMessage("invite me, and let me be your waifu ^-^ http://harmony.exchang3.xyz")
+    },
+    help: 'Sends an invite link for the bot'
+};
