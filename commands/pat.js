3a5b2b1f44a6819941c884828a455840f09dd48d
Trikanoid <herobrine.raw@gmail.com>
2017-07-11 23:27:28 +0800

    Create pat.js

---
+++ commands/pat.js
@@ -0,0 +1,32 @@
+module.exports = {
+    main: function(bot, msg) {
+        var WolkeHelper = require("wolke-api");
+        var WolkeApi = new WolkeHelper({
+            version: 1, //current API versions are 1 and 2 
+            nsfw: false //a boolean for NSFW switching 
+        }); //the constructor arguments are optional 
+        const user = msg.mentions.users.first();
+        WolkeApi.picture("pat").then((result) => {
+            if (user) {
+                const embed = new Discord.RichEmbed()
+                    .setTitle(`${msg.author.username} patted ${user.username}`)
+                    .setColor("#808080")
+                    .setImage(result.url)
+
+                msg.channel.sendEmbed(embed)
+            }
+            if (!user) {
+                const embed = new Discord.RichEmbed()
+                    .setTitle(`Harmony patted the sad and small ${msg.author.username}`)
+                    .setColor("#808080")
+                    .setImage(result.url)
+
+                msg.channel.sendEmbed(embed)
+            }
+        }).catch((error) => {
+            console.log(error.toString());
+        })
+    },
+    args: '<mention>',
+    help: 'Give a headpat to the lonely people'
+};
