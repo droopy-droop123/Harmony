97828b8451824e52f34f4cc29c6856e798b1890a
Trikanoid <herobrine.raw@gmail.com>
2017-07-11 23:15:37 +0800

    Formatted code to make sexy

--- commands/feed.js
+++ commands/feed.js
@@ -6,28 +6,28 @@ module.exports = {
             nsfw: false //a boolean for NSFW switching 
         }); //the constructor arguments are optional 
         const user = message.mentions.users.first();
-    WolkeApi.picture("nom").then((result) => {
-        if (user) {
-            const embed = new Discord.RichEmbed()
-                .setTitle(`${message.author.username} fed ${user.username} some good noms`)
-                .setColor("#808080")
-                .setImage(result.url)
-                .setFooter('©2017 Felix#1330', 'http://orig13.deviantart.net/f7a2/f/2016/343/a/b/isana_yashiro_minimal_icon_by_lol123xb-dar48hx.jpg')
+        WolkeApi.picture("nom").then((result) => {
+            if (user) {
+                const embed = new Discord.RichEmbed()
+                    .setTitle(`${message.author.username} fed ${user.username} some good noms`)
+                    .setColor("#808080")
+                    .setImage(result.url)
+                    .setFooter('©2017 Felix#1330', 'http://orig13.deviantart.net/f7a2/f/2016/343/a/b/isana_yashiro_minimal_icon_by_lol123xb-dar48hx.jpg')
 
-            message.channel.sendEmbed(embed)
-        }
-        if (!user) {
-            const embed = new Discord.RichEmbed()
-                .setTitle(`Tsuda fed the lonely ${message.author.username} some sad noms`)
-                .setColor("#808080")
-                .setImage(result.url)
-                .setFooter('©2017 Felix#1330', 'http://orig13.deviantart.net/f7a2/f/2016/343/a/b/isana_yashiro_minimal_icon_by_lol123xb-dar48hx.jpg')
+                message.channel.sendEmbed(embed)
+            }
+            if (!user) {
+                const embed = new Discord.RichEmbed()
+                    .setTitle(`Tsuda fed the lonely ${message.author.username} some sad noms`)
+                    .setColor("#808080")
+                    .setImage(result.url)
+                    .setFooter('©2017 Felix#1330', 'http://orig13.deviantart.net/f7a2/f/2016/343/a/b/isana_yashiro_minimal_icon_by_lol123xb-dar48hx.jpg')
 
-            message.channel.sendEmbed(embed)
-        }
-    }).catch((error) => {
-        console.log(error.toString());
-    })
+                message.channel.sendEmbed(embed)
+            }
+        }).catch((error) => {
+            console.log(error.toString());
+        })
     },
     args: '<mention>',
     help: 'Give food to someone plox'
