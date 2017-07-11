06cffc57959ea0432fa63234f77e6fbf84fc9895
ExChang3 <worshipleader107@gmail.com>
2017-07-11 02:51:28 -0500

    Delete support.js

--- support.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-     main: function(bot, msg) {
-        msg.channel.sendMessage("Here's A Link To Our Support Server: http://theserv.exchang3.xyz")
-    },
-    help: 'Gives A Link For The Bots Support Server'
-};
