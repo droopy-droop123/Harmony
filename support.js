40a9b42e7adc6143e5681c124e76d545d9f63bc3
ExChang3 <worshipleader107@gmail.com>
2017-07-11 02:47:21 -0500

    Add files via upload

---
+++ support.js
@@ -0,0 +1,6 @@
+module.exports = {
+     main: function(bot, msg) {
+        msg.channel.sendMessage("Here's A Link To Our Support Server: http://theserv.exchang3.xyz")
+    },
+    help: 'Gives A Link For The Bots Support Server'
+};
