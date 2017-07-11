47608aade96363bafd097de4f8d222b5b716e426
Trikanoid <herobrine.raw@gmail.com>
2017-07-11 23:02:54 +0800

    Update hug.js

--- commands/hug.js
+++ commands/hug.js
@@ -1,26 +1,31 @@
 module.exports = {
-     main: function(bot, msg) {
-     const user = message.mentions.users.first();
-    WolkeApi.picture("hug").then((result) => {
-        if (user) {
-            const embed = new Discord.RichEmbed()
-                .setTitle(`${message.author.username} hugged ${user.username}`)
-                .setColor("#808080")
-                .setImage(result.url)
+    main: function(bot, msg) {
+        var WolkeHelper = require("wolke-api");
+        var WolkeApi = new WolkeHelper({
+            version: 1, //current API versions are 1 and 2 
+            nsfw: false //a boolean for NSFW switching 
+        }); //the constructor arguments are optional 
+        const user = message.mentions.users.first();
+        WolkeApi.picture("hug").then((result) => {
+            if (user) {
+                const embed = new Discord.RichEmbed()
+                    .setTitle(`${message.author.username} hugged ${user.username}`)
+                    .setColor("#808080")
+                    .setImage(result.url)
 
-            message.channel.sendEmbed(embed)
-        }
-        if (!user) {
-            const embed = new Discord.RichEmbed()
-                .setTitle(`Tsuda hugged the lonely ${message.author.username}`)
-                .setColor("#808080")
-                .setImage(result.url)
+                message.channel.sendEmbed(embed)
+            }
+            if (!user) {
+                const embed = new Discord.RichEmbed()
+                    .setTitle(`Tsuda hugged the lonely ${message.author.username}`)
+                    .setColor("#808080")
+                    .setImage(result.url)
 
-            message.channel.sendEmbed(embed)
-        }
-    }).catch((error) => {
-        console.log(error.toString());
-    })
+                message.channel.sendEmbed(embed)
+            }
+        }).catch((error) => {
+            console.log(error.toString());
+        })
     },
     args: '<mention>',
     help: 'Hug a user'
