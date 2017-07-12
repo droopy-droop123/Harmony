acc9904038246bbf10892de48e3eedc4f0ad151e
Trikanoid <herobrine.raw@gmail.com>
2017-07-12 18:20:26 +0800

    Added permissions

--- commands/ban.js
+++ commands/ban.js
@@ -1,23 +1,28 @@
 module.exports = {
-     main: function(bot, msg) {
-     const mention = msg.mentions.users.first()
-	var banPerms = msg.guild.member(bot.user).hasPermission('BAN_MEMBERS')
-	if (!mention) {
-		msg.channel.send('Please mention a user, that you would like to ban.')
-	} else {
-		if (!banPerms) {
-			msg.channel.send("Harmony doesn't have enough permissions to ban any user.")
-		} else {
-			var bannable = msg.guild.member(mention).bannable
-			if (!bannable) {
-				msg.channel.send(mention + " isn't bannable.")
-			} else {
-				msg.guild.ban(mention)
-				msg.channel.send(mention + ' has been banned.')
-				}
-			}
-   		}
-	},
+    main: function(bot, msg) {
+        if (msg.member.hasPermission("BAN_MEMBERS") == true) {
+            const mention = msg.mentions.users.first()
+            var banPerms = msg.guild.member(bot.user).hasPermission('BAN_MEMBERS')
+            if (!mention) {
+                msg.channel.send('Please mention a user, that you would like to ban.')
+            }
+            else {
+                if (!banPerms) {
+                    msg.channel.send("Harmony doesn't have enough permissions to ban any user.")
+                }
+                else {
+                    var bannable = msg.guild.member(mention).bannable
+                    if (!bannable) {
+                        msg.channel.send(mention + " isn't bannable.")
+                    }
+                    else {
+                        msg.guild.ban(mention)
+                        msg.channel.send(mention + ' has been banned.')
+                    }
+                }
+            }
+        }
+    },
     args: '<mention>',
     help: 'Ban a user'
 };
--- commands/kick.js
+++ commands/kick.js
@@ -1,22 +1,27 @@
 module.exports = {
-     main: function(bot, msg) {
-     const mention = msg.mentions.users.first();
-	var kickPerms = msg.guild.member(bot.user).hasPermission('KICK_MEMBERS')
-	if (!mention) {
-		msg.channel.send('Please mention a user, that you would like to kick.')
-	} else {
-		if (!kickPerms) {
-			msg.channel.send("Harmony doesn't have enough permissions to kick any user.")
-		} else {
-			var kickable = msg.guild.member(mention).kickable
-			if (!kickable) {
-				msg.channel.send(mention + " isn't kickable.")
-			} else {
-				msg.guild.member(mention).kick()
-				msg.channel.send(mention + ' has been kicked.')
-			}
-		}
-	}
+    main: function(bot, msg) {
+        if (msg.member.hasPermission("KICK_MEMBERS") == true) {
+            const mention = msg.mentions.users.first();
+            var kickPerms = msg.guild.member(bot.user).hasPermission('KICK_MEMBERS')
+            if (!mention) {
+                msg.channel.send('Please mention a user, that you would like to kick.')
+            }
+            else {
+                if (!kickPerms) {
+                    msg.channel.send("Harmony doesn't have enough permissions to kick any user.")
+                }
+                else {
+                    var kickable = msg.guild.member(mention).kickable
+                    if (!kickable) {
+                        msg.channel.send(mention + " isn't kickable.")
+                    }
+                    else {
+                        msg.guild.member(mention).kick()
+                        msg.channel.send(mention + ' has been kicked.')
+                    }
+                }
+            }
+        }
     },
     args: '<mention>',
     help: 'Kick a user'
