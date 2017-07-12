9cbf36610af24a04f1327ee6fc4c747bb36b8ac2
Trikanoid <herobrine.raw@gmail.com>
2017-07-12 11:28:22 +0800

    Create pout.js

---
+++ commands/pout.js
@@ -0,0 +1,22 @@
+module.exports = {
+    main: function(bot, msg) {
+        var Discord = require('discord.js')
+        var WolkeHelper = require("wolke-api");
+        var WolkeApi = new WolkeHelper({
+            version: 1, //current API versions are 1 and 2 
+            nsfw: false //a boolean for NSFW switching 
+        }); //the constructor arguments are optional 
+        WolkeApi.picture("pout").then((result) => {
+            const embed = new Discord.RichEmbed()
+                .setTitle(`${msg.author.username} pouts`)
+                .setColor("#808080")
+                .setImage(result.url)
+
+            msg.channel.sendEmbed(embed)
+        }).catch((error) => {
+            console.log(error.toString());
+        })
+    },
+    args: '<mention>',
+    help: 'O )3 O'
+};
