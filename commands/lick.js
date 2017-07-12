72f69e8500e79ae3d398bbd74b74f6876d7492bb
Trikanoid <herobrine.raw@gmail.com>
2017-07-12 11:23:16 +0800

    Create lick.js

---
+++ commands/lick.js
@@ -0,0 +1,33 @@
+module.exports = {
+    main: function(bot, msg) {
+        var Discord = require('discord.js')
+        var WolkeHelper = require("wolke-api");
+        var WolkeApi = new WolkeHelper({
+            version: 1, //current API versions are 1 and 2 
+            nsfw: false //a boolean for NSFW switching 
+        }); //the constructor arguments are optional 
+        const user = msg.mentions.users.first();
+        WolkeApi.picture("lick").then((result) => {
+            if (user) {
+                const embed = new Discord.RichEmbed()
+                    .setTitle(`${msg.author.username} licked ${user.username}`)
+                    .setColor("#808080")
+                    .setImage(result.url)
+
+                msg.channel.sendEmbed(embed)
+            }
+            if (!user) {
+                const embed = new Discord.RichEmbed()
+                    .setTitle(`Harmony licked the dry ${msg.author.username}`)
+                    .setColor("#808080")
+                    .setImage(result.url)
+
+                msg.channel.sendEmbed(embed)
+            }
+        }).catch((error) => {
+            console.log(error.toString());
+        })
+    },
+    args: '<mention>',
+    help: 'Lickity lick lick'
+};
